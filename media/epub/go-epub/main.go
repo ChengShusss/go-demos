7bcdd96431d53d527dcd0c28dcc835187551c663
ChengShusss <cheng.s.shu@foxmail>
2023-10-30 03:24:49 +0800

    manual commit at 20231030

--- media/epub/go-epub/main.go
+++ media/epub/go-epub/main.go
@@ -14,34 +14,18 @@ func main() {
 		log.Println(err)
 	}
 
-	// Set the author
-	e.SetAuthor("Hingle McCringleberry")
-
 	// Add a section
-	section1Body := `<h1>Section 1</h1>
+	sectionBody := `<h1>Section 1</h1>
 	<p>This is a paragraph.</p>`
-	parent1, err := e.AddSection(section1Body, "Section 1", "", "")
+	parent1, err := e.AddSection(sectionBody, "Section 1", "", "")
 	if err != nil {
 		log.Println(err)
 	}
 
-	fmt.Printf("Add Section\n")
-
-	// Add a section
-	section11Body := `<h2>Section 1-1</h2>
+	// Add a subsection
+	subsectionBody := `<h2>Section 1-1</h2>
 	<p>This is a paragraph of h2.</p>`
-	parent11, err := e.AddSubSection(parent1, section11Body, "Section 1-1", "SubSectionName", "")
-	if err != nil {
-		log.Println(err)
-	}
-
-	fmt.Printf("Add subSection\n")
-	fmt.Printf("SubSection name: %v\n", parent11)
-
-	// Add a section
-	section111Body := `<h3>Section 1-1-1</h3>
-	<p>This is a paragraph of h3.</p>`
-	_, err = e.AddSubSection(parent11, section111Body, "Section 1-1-1", "", "")
+	_, err = e.AddSubSection(parent1, subsectionBody, "Section 1-1", "SubSectionName", "")
 	if err != nil {
 		log.Println(err)
 	}
