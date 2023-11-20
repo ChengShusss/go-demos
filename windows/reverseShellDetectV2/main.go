1e64184f45dfbda9cedd30ee018b10b38de30a7f
Shu Cheng(程澍) <shucheng@Hillstonenet.com>
2023-11-20 17:42:33 +0800

    in progress save: need to wraper process(add enrich func)

--- windows/reverseShellDetectV2/main.go
+++ windows/reverseShellDetectV2/main.go
@@ -3,30 +3,17 @@ package main
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 	"strconv"
-	"strings"
-	"syscall"
 	"time"
 
-	"github.com/shirou/gopsutil/process"
-	"golang.org/x/sys/windows"
-)
-
-var (
-	modKernelBase            = windows.NewLazySystemDLL("Kernelbase.dll")
-	procCompareObjectHandles = modKernelBase.NewProc("CompareObjectHandles")
-
-	ShellList = map[string]bool{
-		"cmd.exe":        true,
-		"powershell.exe": true,
-	}
+	"github.com/chengshusss/go-demos/windows/reverseShellDetectV2/reverseshell"
 )
 
 func main() {
 	// Demo()
 
 	start := time.Now()
+	defer func() { fmt.Printf("consumed: %v ms\n", time.Since(start).Milliseconds()) }()
 	var pids []int32
 	if len(os.Args) > 1 {
 		for _, arg := range os.Args[1:] {
@@ -35,137 +22,19 @@ func main() {
 				fmt.Printf("Invalid pid: %v\n", arg)
 				os.Exit(1)
 			}
-			pids = append(pids, int32(pid))
+			if reverseshell.DetectByPid(int32(pid)) {
+				fmt.Printf("%d is reverse shell\n", pid)
+			}
 		}
+		return
 	}
 
-	processes, err := LoadProcess(pids)
+	processes, err := reverseshell.LoadProcess(pids)
 	if err != nil {
 		fmt.Printf("Failed to get process, err: %v\n", err)
 		os.Exit(1)
 	}
 
 	// fmt.Printf("Len of process: %v\n", processes)
-	DetectReverseShell(processes)
-
-	fmt.Printf("consumed: %v ms\n", time.Since(start).Milliseconds())
-}
-
-func DetectReverseShell(processes []*process.Process) {
-	for _, process := range processes {
-		exe, err := process.Exe()
-		if err != nil {
-			// fmt.Printf("Failed to get process %v, err: %v\n", process.Pid, err)
-			continue
-		}
-		if !isShell(exe) {
-			// fmt.Printf("Process %v is not shell, continue\n", process.Pid)
-			continue
-		}
-
-		isReverse := checkProcessTree(process)
-		if isReverse {
-			fmt.Printf("%v, - %v\n", process.Pid, isReverse)
-		}
-	}
-}
-
-func checkProcessTree(current *process.Process) bool {
-
-	var nodeList []*process.Process
-	nodeFilter := map[uintptr]bool{uintptr(current.Pid): true}
-
-	parent := current
-	var err error
-	for {
-		parent, err = parent.Parent()
-		if err != nil {
-			break
-		}
-		nodeList = append(nodeList, parent)
-		nodeFilter[uintptr(parent.Pid)] = true
-	}
-
-	pidHandleMap := TransHandle(GetHandlesForProcess(nodeFilter))
-
-	for _, node := range nodeList {
-		cond1 := DetectHandle(
-			pidHandleMap[uintptr(node.Pid)],
-			pidHandleMap[uintptr(current.Pid)],
-		)
-		conns, err := node.Connections()
-		if err != nil {
-			continue
-		}
-		cond2 := len(conns) > 0
-		if cond1 && cond2 {
-			for _, c := range conns {
-				fmt.Printf("%+v\n", c)
-			}
-			return true
-		}
-	}
-
-	return false
-}
-
-// 检查两个进程是否有共用的pipe或socket对象
-func DetectHandle(h1, h2 []windows.Handle) bool {
-
-	for _, handle1 := range h1 {
-
-		type1, err := windows.GetFileType(handle1)
-		if err != nil {
-			continue
-		}
-		if type1 != 3 && type1 != 2 {
-			continue
-		}
-
-		for _, handle2 := range h2 {
-
-			type2, err := windows.GetFileType(handle2)
-			if err != nil {
-				continue
-			}
-			if type2 != 3 && type2 != 2 {
-				continue
-			}
-
-			if CompareObjectHandles(handle1, handle2) {
-				return true
-			}
-		}
-	}
-
-	return false
-}
-
-func CompareObjectHandles(handle1, handle2 windows.Handle) bool {
-	result, _, _ := syscall.SyscallN(procCompareObjectHandles.Addr(), uintptr(handle1), uintptr(handle2))
-	return result == 1
-}
-
-func LoadProcess(pids []int32) ([]*process.Process, error) {
-
-	if len(pids) == 0 {
-		processes, err := process.Processes()
-		return processes, err
-	}
-
-	var processes []*process.Process
-	for _, pid := range pids {
-		process, err := process.NewProcess(pid)
-		if err != nil {
-			return nil, err
-		}
-		processes = append(processes, process)
-	}
-
-	return processes, nil
-}
-
-func isShell(path string) bool {
-	exe := filepath.Base(path)
-	return ShellList[strings.ToLower(exe)]
+	reverseshell.DetectProcesses(processes)
 }
---
+++ windows/reverseShellDetectV2/reverseshell/main_test.go
@@ -0,0 +1,72 @@
+package reverseshell
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/shirou/gopsutil/process"
+	"golang.org/x/sys/windows"
+)
+
+func TestCompate(t *testing.T) {
+	// 获取进程句柄
+	handle1, err := windows.OpenProcess(windows.PROCESS_DUP_HANDLE, false, 21456)
+	if err != nil {
+		fmt.Println("Error opening process handle 1:", err)
+		return
+	}
+	defer windows.CloseHandle(handle1)
+
+	handle2, err := windows.OpenProcess(windows.PROCESS_DUP_HANDLE, false, 18332)
+	if err != nil {
+		fmt.Println("Error opening process handle 2:", err)
+		return
+	}
+	defer windows.CloseHandle(handle2)
+
+	// 比较句柄
+	equal := compareObjectHandles(handle1, handle2)
+
+	if equal {
+		fmt.Println("The two processes have the same handle.")
+	} else {
+		fmt.Println("The two processes do not have the same handle.")
+	}
+}
+
+func TestOpenFiles(t *testing.T) {
+	process, _ := process.NewProcess(21456)
+	fds, _ := process.OpenFiles()
+	name, _ := process.Name()
+	process.Rlimit()
+	fmt.Printf("Names: %v\n", name)
+	for _, fd := range fds {
+		fmt.Printf("Name: %v, fd: %+v\n", name, fd)
+	}
+}
+
+func TestConnections(t *testing.T) {
+	process, _ := process.NewProcess(21456)
+	conns, _ := process.Connections()
+	name, _ := process.Name()
+	fmt.Printf("Names: %v\n", name)
+	for _, c := range conns {
+		fmt.Printf("  connetions: %+v\n", c)
+	}
+}
+
+func TestDectect(t *testing.T) {
+	var pid1, pid2 int32 = 21456, 18332
+	p1, _ := process.NewProcess(pid1)
+	p2, _ := process.NewProcess(pid2)
+	nodeFilter := map[uintptr]bool{uintptr(pid1): true, uintptr(pid2): true}
+
+	pidHandleMap := transHandle(getHandlesForProcess(nodeFilter))
+
+	res := hasSharedHandle(
+		pidHandleMap[uintptr(p1.Pid)],
+		pidHandleMap[uintptr(p2.Pid)],
+	)
+
+	t.Logf("Res: %v\n", res)
+}
